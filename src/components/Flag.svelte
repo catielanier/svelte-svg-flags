5a3f818da832464a9887bc7c4ca8860eead45c71
Catie Lanier <hello@catielanier.ca>
2025-05-22 18:05:04 -0400

    consolidate logic into single component

--- src/components/Flag.svelte
+++ src/components/Flag.svelte
@@ -1,14 +1,35 @@
+
 <script lang="ts">
   import countryCodes from '../utils/countryCodes.ts'
+  import {usCodes, canadaCodes, ukCodes, australiaCodes} from '../utils/stateCodes.ts'
 
-  export let code: string;
+  export let country: string;
+  export let state: string | undefined;
   export let width: number | undefined;
 
   const countryObject = countryCodes.find((x) => {
     return x.alpha2 === code || x.alpha3 === code;
   })
+
+  const getStateName = (): string => {
+    switch (countryObject.alpha2) {
+      case 'us':
+        return usCodes[state]
+      case 'ca':
+        return canadaCodes[state]
+      case 'uk':
+        return ukCodes[state]
+      case 'au':
+        return australiaCodes[state]
+      default:
+        break;
+    }
+  }
+
+  const stateName: string | undefined = state ? getStateName() : undefined;
 </script>
 
 {#if country.alpha2}
-  <img src="../assets/countries/{countryObject.alpha2}.svg" width={width ?? 32} alt={countryObject.name} />
+  <img src={state ? `../assets/states/${country.alpha2}/${state}.svg` : `../assets/countries/${country.alpha2}.svg`} width={width ?? 32} alt={state ? stateName : country.name} />
 {/if}
+
