997ba3e70a487587225e168b5dc2c482d2bb7eac
Catie Lanier <hello@catielanier.ca>
2025-05-22 14:49:33 -0400

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,9 @@
+import { sveltePreprocess } from "svelte-preprocess";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+export default {
+  preprocess: [vitePreprocess(), sveltePreprocess()],
+  compilerOptions: {
+    dev: false,
+  },
+};
