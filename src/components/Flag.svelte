1d3221ba91a9a59332c8ad5cef2a533882b884d8
Catie Lanier <hello@catielanier.ca>
2025-05-22 16:37:21 -0400

    create national flag component

---
+++ src/components/Flag.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import countryCodes from '../utils/countryCodes.ts'
+
+  export let code: string;
+  export let width: number | undefined;
+
+  const country = countryCodes.find((x) => {
+    return x.alpha2 === code || x.alpha3 === code;
+  })
+</script>
+
+{#if country.alpha2}
+  <img src="../assets/countries/{country.alpha2}.svg" width={width ?? 32} alt={country.name} />
+{/if}
