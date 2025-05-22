a9d7a1ea34ac0058c85f5233d1775ec542f81b10
Catie Lanier <hello@catielanier.ca>
2025-05-22 19:00:49 -0400

    fix module build

---
+++ src/utils/countryCodes.d.ts
@@ -0,0 +1,6 @@
+declare const countryCodes: readonly {
+    name: string;
+    alpha2: string;
+    alpha3: string;
+}[];
+export default countryCodes;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
+// This is the correct setup for a library, not a web app
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "svelte-svg-icons",
+      fileName: (format) => `svelte-svg-icons.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["svelte"],
+      output: {
+        globals: {
+          svelte: "svelte",
+        },
+      },
+    },
+  },
 });
